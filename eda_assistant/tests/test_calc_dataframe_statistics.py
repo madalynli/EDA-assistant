1ba9e41e828ef20f9a0cfc606172c25beeb96460
madalynli <91808000+madalynli@users.noreply.github.com>
2022-03-13 22:42:37 -0700

    Updated test files
    Updated test files to accommodate for flake8 formatting

--- eda_assistant/tests/test_calc_dataframe_statistics.py
+++ eda_assistant/tests/test_calc_dataframe_statistics.py
@@ -10,7 +10,8 @@ from eda_assistant import _calc_dataframe_statistics
 
 class TestCalcDataFrameStatistics(unittest.TestCase):
     """
-    Tests the dataframe summary statistic calculations in _calc_dataframe_statistics.py
+    Tests the dataframe summary statistic calculations in
+    _calc_dataframe_statistics.py
     """
 
     def setUp(self):
@@ -18,59 +19,65 @@ class TestCalcDataFrameStatistics(unittest.TestCase):
         dataset_path_name = package_file_path + '/data/cereal.csv'
         self.df = pd.read_csv(dataset_path_name)
 
-    def tearDown(self):
-        self.df = None
-
     def test_count_cols(self):
         """
         Tests count_cols function
         """
-        self.assertEqual(_calc_dataframe_statistics.count_cols(self.df), 16)
+        self.assertEqual(_calc_dataframe_statistics.
+                         count_cols(self.df), 16)
 
     def test_count_rows(self):
         """
         Tests count_rows function
         """
-        self.assertEqual(_calc_dataframe_statistics.count_rows(self.df), 77)
+        self.assertEqual(_calc_dataframe_statistics.
+                         count_rows(self.df), 77)
 
     def test_count_total_values(self):
         """
         Tests count_total_values function
         """
-        self.assertEqual(_calc_dataframe_statistics.count_total_values(self.df), 1232)
+        self.assertEqual(_calc_dataframe_statistics.
+                         count_total_values(self.df), 1232)
 
     def test_count_nans(self):
         """
         Tests count_total_nans function
         """
-        self.assertEqual(_calc_dataframe_statistics.count_nans(self.df), 0)
+        self.assertEqual(_calc_dataframe_statistics.
+                         count_nans(self.df), 0)
 
     def test_percent_total_nans(self):
         """
         Tests percent_total_nans function
         """
-        self.assertEqual(_calc_dataframe_statistics.percent_total_nans(self.df), 0)
+        self.assertEqual(_calc_dataframe_statistics.
+                         percent_total_nans(self.df), 0)
 
     def test_count_duplicate_rows(self):
         """
         Tests count_duplicate_rows function
         """
-        self.assertEqual(_calc_dataframe_statistics.count_duplicate_rows(self.df), 0)
+        self.assertEqual(_calc_dataframe_statistics.
+                         count_duplicate_rows(self.df), 0)
 
     def test_percent_duplicate_rows(self):
         """
         Tests percent_duplicate_rows function
         """
-        self.assertEqual(_calc_dataframe_statistics.percent_duplicate_rows(self.df), 0)
+        self.assertEqual(_calc_dataframe_statistics.
+                         percent_duplicate_rows(self.df), 0)
 
     def test_count_numeric(self):
         """
         Tests count_numeric function
         """
-        self.assertEqual(_calc_dataframe_statistics.count_numeric(self.df), 13)
+        self.assertEqual(_calc_dataframe_statistics.
+                         count_numeric(self.df), 13)
 
     def test_count_categorical(self):
         """
         Tests count_count_categorical function
         """
-        self.assertEqual(_calc_dataframe_statistics.count_categorical(self.df), 3)
+        self.assertEqual(_calc_dataframe_statistics.
+                         count_categorical(self.df), 3)
--- eda_assistant/tests/test_create_tables.py
+++ eda_assistant/tests/test_create_tables.py
@@ -10,7 +10,7 @@ from eda_assistant import _create_tables
 TEST_DIR = 'test_create_tables_results'
 
 
-class TestCreateTable(unittest.TestCase):
+class TestCreateTables(unittest.TestCase):
     """
     Tests creating summary statistics tables in _create_tables.py
     """
@@ -22,22 +22,34 @@ class TestCreateTable(unittest.TestCase):
 
     def test_create_df_summary(self):
         """
-        Tests create_df_summary function. This function compares the results of the cereal.csv
-        data set passed through the function to another csv file containing the correct format
-        and values for the df summary table.
+        Tests create_df_summary function. This function compares the results
+        of the cereal.csv dataset passed through the function to another csv
+        file containing the correct format and values for the df summary table.
         """
-        test_create_df_summary_file_name = 'test_create_df_summary_cereal_results.csv'
-        test_data_df_summary = pd.read_csv(TEST_DIR + '/' + test_create_df_summary_file_name, index_col=0)
-        comparison_df_summary = _create_tables.create_df_summary(self.df).astype(str)
-        pd.testing.assert_frame_equal(test_data_df_summary, comparison_df_summary, atol=1)
+        test_create_df_summary_file_name = \
+            'test_create_df_summary_cereal_results.csv'
+        test_data_df_summary = pd.read_csv(TEST_DIR + '/' +
+                                           test_create_df_summary_file_name,
+                                           index_col=0)
+        comparison_df_summary = _create_tables.\
+            create_df_summary(self.df).astype(str)
+        pd.testing.assert_frame_equal(test_data_df_summary,
+                                      comparison_df_summary, atol=0.1)
 
     def test_create_var_summary(self):
         """
-        Tests create_var_summary function. This function compares the results of the cereal.csv
-        data set passed through the function to another csv file containing the correct format
-        and values for the var summary table.
+        Tests create_var_summary function. This function compares the results
+        of the cereal.csv dataset passed through the function to another csv
+        file containing the correct format and values for the var summary
+        table.
         """
-        test_create_var_summary_file_name = 'test_create_var_summary_cereal_results.csv'
-        test_data_var_summary = pd.read_csv(TEST_DIR + '/' + test_create_var_summary_file_name, index_col=0)
-        comparison_var_summary = _create_tables.create_var_summary(self.df).astype(str)
-        pd.testing.assert_frame_equal(test_data_var_summary, comparison_var_summary, atol=1)
+        test_create_var_summary_file_name = \
+            'test_create_var_summary_cereal_results.csv'
+        test_data_var_summary = pd.read_csv(TEST_DIR + '/' +
+                                            test_create_var_summary_file_name,
+                                            index_col=0)
+        comparison_var_summary = _create_tables.\
+            create_var_summary(self.df).astype(str)
+        pd.testing.assert_frame_equal(test_data_var_summary,
+                                      comparison_var_summary,
+                                      atol=0.1)
