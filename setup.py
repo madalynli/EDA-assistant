e2560898dc5c756058c0835c394c7ed23820f955
madalynli <91808000+madalynli@users.noreply.github.com>
2022-03-13 22:51:42 -0700

    Update setup
    Updated formatting and version

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='EDA-assistant',
-    version='0.0.2',
+    version='0.0.4',
     author='Madalyn Li',
     author_email='mli2324@uw.edu',
-    description='This package allows users to create an exploratory data analysis pdf report from any data set',
+    description='This package allows users to create an exploratory data '
+                'analysis pdf report from any data set',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/madalynli/EDA-assistant',
