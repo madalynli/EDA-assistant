97da0f1519bd6da08ce51b47d2911660a0c94093
madalynli <91808000+madalynli@users.noreply.github.com>
2022-03-14 19:31:46 -0700

    Update test_format_graphs.py

--- eda_assistant/tests/test_format_graphs.py
+++ eda_assistant/tests/test_format_graphs.py
@@ -14,8 +14,7 @@ class TestFormatGraphs(unittest.TestCase):
     """
 
     def setUp(self):
-        package_file_path = os.path.dirname(os.path.dirname(os.getcwd()))
-        dataset_path_name = package_file_path + '/data/cereal.csv'
+        dataset_path_name = os.getcwd() + '/data/cereal.csv'
         self.df = pd.read_csv(dataset_path_name)
 
     def test_is_categorical_bar(self):
